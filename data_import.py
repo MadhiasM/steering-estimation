6c427ab81d2b43fcb82a64af39ea9413e88dea67
Mathias Moog <mathias.moog1001@googlemail.com>
2020-04-14 23:34:12 +0200

    commit

--- data_import.py
+++ data_import.py
@@ -25,7 +25,6 @@ df = pd.read_csv('test_data.csv')
 
 # print(df[0:5])
 
-
 # Strip non-numerics
 # print(df.dtypes)
 df = df.select_dtypes(include=['float64', 'int64'])
@@ -46,16 +45,27 @@ df = df.select_dtypes(include=['float64', 'int64'])
 #     print(fields)
 
 # Pandas to Numpy
-
 x = df.drop('oversteer', axis=1).values # Input data
-
 y = df['oversteer'].values  # Output data
 
 # Build the Neural Network
-
 model = Sequential()
 model.add(Dense(25, input_dim=x.shape[1], activation='relu'))  # Hidden 1
 model.add(Dense(10, activation='relu'))                        # Hidden 2
 model.add(Dense(1))                                            # Output
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(x,y,verbose=2,epochs=100)
+
+plot_model(model, to_file='model.png')
+
+pred = model.predict(x)
+print(f"Shape: {pred.shape}")
+print(pred[0:10])
+
+# Measure RMSE error.  RMSE is common for regression.
+score = np.sqrt(metrics.mean_squared_error(pred,y))
+print(f"Final score (RMSE): {score}")
+
+# Sample predictions
+for i in range(10):
+    print(f"{i+1}. Input Data: {x[i]}, Ground Truth Data: {y[i]}, Predicted Output Data: {pred[i]}")
--- google_colab_test.py
+++ google_colab_test.py
@@ -2,19 +2,18 @@
 data import of test_data.csv using pandas
 """
 
-# https://colab.research.google.com/drive/1jQXI2rnhFuNpdEHBPr1nVOZr0KLHZooT
+# Google Colab Link: https://colab.research.google.com/drive/1jQXI2rnhFuNpdEHBPr1nVOZr0KLHZooT
 
 import os
 
-    import numpy
+import numpy as np
 import pandas as pd
 from sklearn import metrics
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.utils import plot_model
 
-# print("The Current directory is " + os.getcwd())
-# os.chdir("./data")
-# print("The Current directory is " + os.getcwd())
+# Get CSV data from GitHub
 
 url = 'https://raw.githubusercontent.com/MadhiasM/steering-estimation/master/data/test_data.csv'
 df = pd.read_csv(url)
@@ -34,4 +33,16 @@ model.add(Dense(1))                                            # Output
 model.compile(loss='mean_squared_error', optimizer='adam')
 model.fit(x,y,verbose=2,epochs=100)
 
-# 
+plot_model(model, to_file='model.png')
+
+pred = model.predict(x)
+print(f"Shape: {pred.shape}")
+print(pred[0:10])
+
+# Measure RMSE error.  RMSE is common for regression.
+score = np.sqrt(metrics.mean_squared_error(pred,y))
+print(f"Final score (RMSE): {score}")
+
+# Sample predictions
+for i in range(10):
+    print(f"{i+1}. Input Data: {x[i]}, Ground Truth Data: {y[i]}, Predicted Output Data: {pred[i]}")
